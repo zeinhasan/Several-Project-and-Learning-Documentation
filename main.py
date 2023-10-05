b80d81ae3f28ebd173345d0f5cf73b9ec1a706bf
zeinhasan <ahmad.habib.hasan@mail.ugm.ac.id>
2023-10-05 08:59:14 +0700

    Add

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ for i in range(1,365*2):
 
     for j in range (0, randint(1,10)):
         d = str(i) + " days ago"
-        with open("data.txt", "a") as myfile:
+        with open("data.txt", "a") as file:
             file.write(d)
         os.system("git add . ")
         os.system("git commit --date='" + d + "' -m 'commit'")
